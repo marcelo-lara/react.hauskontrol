7993dfeed43d5b8f1edd423b669a5f5ca49a1b55
Marcelo Lara <marcelo.lara@globant.com>
2021-01-03 12:40:00 -0300

    switchbutton

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SwitchButton } from "../button/button";
+import { SwitchButton } from "../switchbutton/switchbutton";
 
 export class App extends React.Component {
   render() {
